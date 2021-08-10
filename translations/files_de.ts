c48f01d7a532cdc1391a6505e14c625b342bd90f
Liri CI <ci@liri.io>
2021-08-10 00:20:35 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/files_de.ts
@@ -0,0 +1,288 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<context>
+    <name>FolderListView</name>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="64"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="72"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="80"/>
+        <source>Last modified</source>
+        <translation>Zuletzt geändert</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="121"/>
+        <source>Directories</source>
+        <translation>Verzeichnisse</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="122"/>
+        <source>Files</source>
+        <translation>Dateien</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/FolderListView.qml" line="139"/>
+        <source>No files</source>
+        <translation>Keine Dateien</translation>
+    </message>
+</context>
+<context>
+    <name>FolderModel</name>
+    <message>
+        <location filename="../src/app/qml/backend/FolderModel.qml" line="169"/>
+        <source>Home</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/app/qml/backend/FolderModel.qml" line="240"/>
+        <source>%1 file</source>
+        <translation>%1 Datei</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/backend/FolderModel.qml" line="241"/>
+        <source>%1 files</source>
+        <translation>%1 Dateien</translation>
+    </message>
+</context>
+<context>
+    <name>FolderPage</name>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="36"/>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="37"/>
+        <source>Go back</source>
+        <translation>Zurückgehen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="48"/>
+        <location filename="../src/app/qml/FolderPage.qml" line="208"/>
+        <source>Search</source>
+        <translation>Suchen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="49"/>
+        <source>Search files or folders</source>
+        <translation>Dateien oder Ordner suchen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="60"/>
+        <source>New folder</source>
+        <translation>Neuer Ordner</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="61"/>
+        <source>Create a new folder</source>
+        <translation>Einen neuen Ordner erstellen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="67"/>
+        <location filename="../src/app/qml/FolderPage.qml" line="68"/>
+        <source>Paste</source>
+        <translation>Einfügen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="90"/>
+        <location filename="../src/app/qml/FolderPage.qml" line="91"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="233"/>
+        <source>Create new folder:</source>
+        <translation>Neuen Ordner erstellen:</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/FolderPage.qml" line="246"/>
+        <source>New Folder</source>
+        <translation>Neuer Ordner</translation>
+    </message>
+</context>
+<context>
+    <name>InfoSidebar</name>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="58"/>
+        <source>Edited </source>
+        <translation>Bearbeitet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="67"/>
+        <source>Share</source>
+        <translation>Teilen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="71"/>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="77"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="76"/>
+        <source>Are you sure you want to permanently delete &quot;%1&quot;?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="107"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="127"/>
+        <source>Location</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="138"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="156"/>
+        <source>Contents</source>
+        <translation>Inhalt</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/InfoSidebar.qml" line="156"/>
+        <source>Size</source>
+        <translation>Größe</translation>
+    </message>
+</context>
+<context>
+    <name>PlacesSidebar</name>
+    <message>
+        <location filename="../src/app/qml/components/PlacesSidebar.qml" line="34"/>
+        <source>Places</source>
+        <translation>Orte</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/components/PlacesSidebar.qml" line="55"/>
+        <source>Trash</source>
+        <translation>Papierkorb</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionPage</name>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="29"/>
+        <source>Select files...</source>
+        <translation>Dateien auswählen ...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="33"/>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="45"/>
+        <source>Cut</source>
+        <translation>Ausschneiden</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="51"/>
+        <source>Copy</source>
+        <translation>Kopieren</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="57"/>
+        <source>Paste</source>
+        <translation>Einfügen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="64"/>
+        <source>Move to Trash</source>
+        <translation>In Papierkorb verschieben</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="70"/>
+        <source>Select all</source>
+        <translation>Alles auswählen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SelectionPage.qml" line="76"/>
+        <source>Clear selection</source>
+        <translation>Auswahl aufheben</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="25"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="40"/>
+        <source>Show hidden files</source>
+        <translation>Versteckte Dateien anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="53"/>
+        <source>Sort by:</source>
+        <translation>Sortieren nach:</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="57"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="57"/>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="63"/>
+        <source>Sort order:</source>
+        <translation>Sortierung:</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="67"/>
+        <source>Ascending</source>
+        <translation>Aufsteigend</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/dialogs/SettingsDialog.qml" line="67"/>
+        <source>Descending</source>
+        <translation>Absteigend</translation>
+    </message>
+</context>
+<context>
+    <name>TrashPage</name>
+    <message>
+        <location filename="../src/app/qml/TrashPage.qml" line="28"/>
+        <source>Trash</source>
+        <translation>Papierkorb</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/TrashPage.qml" line="32"/>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/TrashPage.qml" line="45"/>
+        <source>Empty trash</source>
+        <translation>Papierkorb leeren</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/TrashPage.qml" line="50"/>
+        <source>Restore trash</source>
+        <translation>Papierkorb wiederherstellen</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="34"/>
+        <source>Files</source>
+        <translation>Dateien</translation>
+    </message>
+</context>
+</TS>
